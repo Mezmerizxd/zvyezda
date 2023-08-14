b1e4b75d823952b306069127548b507d45ffe615
Mezmerizxd <rorypearson2003@hotmail.com>
2023-08-14 22:25:43 +0100

    feat(surveillance): sql

---
+++ prisma/migrations/20230814103847_surveillance/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Surveillance" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Surveillance_pkey" PRIMARY KEY ("id")
+);
