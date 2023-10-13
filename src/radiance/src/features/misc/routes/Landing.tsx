f51fb7fee012bbe3f26f41aec6d41de32b0b1d02
Mezmerizxd <rorypearson2003@hotmail.com>
2023-10-13 13:04:50 +0100

    feat(radiance): Landing page

--- src/radiance/src/features/misc/routes/Landing.tsx
+++ src/radiance/src/features/misc/routes/Landing.tsx
@@ -45,80 +45,76 @@ export const Landing = () => {
             <h2 className="py-2 px-5 text-4xl font-extrabold text-white-light w-4/6 m-auto">
               Choose the right plan for you
             </h2>
-            <p className="px-5 font-normal text-white-dark w-4/6 m-auto">
-              Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas
-              in. Explicabo id ut laborum.
-            </p>
           </div>
-          <div className="grid grid-flow-row-dense grid-cols-3">
+          <div className="flex justify-center">
             <div className="mx-4 my-4 px-4 py-4 w-fit border border-white-dark/20 rounded-md">
-              <p className="text-left font-medium text-white-light w-fit">Basic</p>
-              <h2 className="my-2 text-left font-medium text-4xl text-white-light w-fit">£15</h2>
-              <h3 className="text-left font-medium text-white-light w-fit">Some very informative information here.</h3>
+              <p className="text-left font-medium text-white-light w-fit">Quick</p>
+              <h2 className="my-2 text-left font-medium text-4xl text-white-light w-fit">£30</h2>
+              <h3 className="text-left font-medium text-white-light w-fit">A Quick and Sharp Clean.</h3>
               <div className="my-4 w-fit">
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Floor Cleaning</p>
                 </div>
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Window Cleaning</p>
                 </div>
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
-                </div>
-                <div className="my-2 flex items-center w-fit">
-                  <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Dusting</p>
                 </div>
               </div>
             </div>
 
             <div className="mx-4 my-4 px-4 py-4 w-fit border border-white-dark/20 rounded-md">
-              <p className="text-left font-medium text-white-light w-fit">Basic</p>
-              <h2 className="my-2 text-left font-medium text-4xl text-white-light w-fit">£15</h2>
-              <h3 className="text-left font-medium text-white-light w-fit">Some very informative information here.</h3>
+              <p className="text-left font-medium text-white-light w-fit">Normal</p>
+              <h2 className="my-2 text-left font-medium text-4xl text-white-light w-fit">£45</h2>
+              <h3 className="text-left font-medium text-white-light w-fit">A More In-Depth Clean.</h3>
               <div className="my-4 w-fit">
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Floor Cleaning</p>
                 </div>
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Window Cleaning</p>
                 </div>
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Dusting</p>
                 </div>
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Washed Dishes</p>
                 </div>
               </div>
             </div>
 
             <div className="mx-4 my-4 px-4 py-4 w-fit border border-white-dark/20 rounded-md">
-              <p className="text-left font-medium text-white-light w-fit">Basic</p>
-              <h2 className="my-2 text-left font-medium text-4xl text-white-light w-fit">£15</h2>
-              <h3 className="text-left font-medium text-white-light w-fit">Some very informative information here.</h3>
+              <p className="text-left font-medium text-white-light w-fit">Extra</p>
+              <h2 className="my-2 text-left font-medium text-4xl text-white-light w-fit">£60</h2>
+              <h3 className="text-left font-medium text-white-light w-fit">Deep Clean</h3>
               <div className="my-4 w-fit">
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Floor Cleaning</p>
                 </div>
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Window Cleaning</p>
                 </div>
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Dusting</p>
                 </div>
                 <div className="my-2 flex items-center w-fit">
                   <AiFillCheckCircle className="text-radiance-light" size={26} />
-                  <p className="ml-2 text-left font-normal text-white-dark">Some very informative information here.</p>
+                  <p className="ml-2 text-left font-normal text-white-dark">Washed Dishes</p>
+                </div>
+                <div className="my-2 flex items-center w-fit">
+                  <AiFillCheckCircle className="text-radiance-light" size={26} />
+                  <p className="ml-2 text-left font-normal text-white-dark">House Flipped Upside Down</p>
                 </div>
               </div>
             </div>
